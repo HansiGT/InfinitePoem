b863cfd39a2f1a40151995753b254b242453da90
Johannes Ratz <ratz.hannes@googlemail.com>
2020-10-10 03:38:39 +0200

    trimmer works, added 109 elements in silent

--- TrimSilence.py
+++ TrimSilence.py
@@ -4,16 +4,16 @@ import argparse
 from pydub import AudioSegment
 from pydub.silence import detect_leading_silence
 
-for (dirpath, dirnames, filenames) in os.walk("tracks (uncut)2/"):
+for (dirpath, dirnames, filenames) in os.walk("silent"):
     for filename in filenames:
         filepath = dirpath + '/' + filename
         try:
             track = AudioSegment.from_file(filepath)
-            x = - (len(track) - detect_leading_silence(track, 0.01))
-            track = track[x:]
-            y = len(track) - detect_leading_silence(track.reverse(), 0.01)
-            track = track[:y]
+            x = - (len(track) - detect_leading_silence(track)) - 80
+            track1 = track[x:]
+            y = len(track1) - detect_leading_silence(track1.reverse()) + 80
+            track2 = track1[:y]
             print('TRIMMING: ' + str(filepath))
-            file_handle = track.export(filepath, format='wav')
+            file_handle = track2.export(filepath, format='wav')
         except:
             print("ERROR TRIMMING " + str(filepath))
