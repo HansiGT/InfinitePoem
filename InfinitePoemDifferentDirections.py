bcf38a112bc4ef4f68c20425e142a5a49bd74164
Johannes Ratz <ratz.hannes@googlemail.com>
2020-10-29 17:10:55 +0100

    new Schallplatten-Sounds

--- InfinitePoemDifferentDirections.py
+++ InfinitePoemDifferentDirections.py
@@ -33,23 +33,34 @@ while True:
     i = randint(0, len(first) - 1)
     j = randint(0, len(second) - 1)
     k = randint(0, 2)
-    l = randint(0, 9)
-
+    l = randint(0, 19)
+    m = randint(0, 19)
+    n = randint(0, 19)
 
     play(0, first[i])
     pygame.time.wait(int(round(first[i].get_length(), 3) * 1000) + 300)
+    if True:
+        pygame.time.wait(500)
+        for a in range(0, randint(1, 3)):
+            play(0, first[i])
+            pygame.time.wait(int(round(first[i].get_length(), 3) * 1000) + 300)
     play(0, second[j])
     pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
-
-    if l == 0:
-        for a in range(0, 4):
+    if n == 0:
+        pygame.time.wait(750)
+        for a in range(0, randint(1, 3)):
             play(0, second[j])
             pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
-    else:
-        while k > 0:
+
+    while k > 0:
             j = randint(0, len(second) - 1)
             play(0, second[j])
             pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
             k = randint(0, 2)
+            if l == 0:
+                for a in range(0, randint(1, 6)):
+                    play(0, second[j])
+                    pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
+            l = randint(0, 19)
 
-    pygame.time.wait(2000)
+    pygame.time.wait(1000)
