e05d8b4d0c86ad3b72f8a4c6d259ad4d17a09536
Johannes Ratz <ratz.hannes@googlemail.com>
2020-10-29 03:45:51 +0100

    updated pygame to version 2.0 (fixed sound pitch), updated different directions by using pygame directly instead of premaking the wav files

--- InfinitePoem.py
+++ InfinitePoem.py
@@ -5,7 +5,7 @@ from random import randint
 pygame.mixer.init()
 pygame.init()
 
-pygame.mixer.set_num_channels(26)
+pygame.mixer.set_num_channels(10)
 
 first = []
 
@@ -22,8 +22,8 @@ for (dirpath, dirnames, filenames) in os.walk("tracks/second/"):
         second.append(pygame.mixer.Sound(filepath))
 
 
-def play(x, channel, track):
-    pygame.mixer.Channel(channel).play(track, loops=0, maxtime=x)
+def play(channel, track):
+    pygame.mixer.Channel(channel).play(track, loops=0)
 
 
 while True:
@@ -32,19 +32,19 @@ while True:
     k = randint(0, 2)
     l = randint(0, 9)
 
-    play(10000, 0, first[i])
+    play(0, first[i])
     pygame.time.wait(int(round(first[i].get_length(), 3) * 1000) + 300)
-    play(10000, 0, second[j])
+    play(0, second[j])
     pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
 
     if l == 0:
         for a in range(0, 4):
-            play(10000, 0, second[j])
+            play(0, second[j])
             pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
     else:
         while k > 0:
             j = randint(0, len(second) - 1)
-            play(10000, 0, second[j])
+            play(0, second[j])
             pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
             k = randint(0, 2)
 
--- InfinitePoemDifferentDirections.py
+++ InfinitePoemDifferentDirections.py
@@ -1,29 +1,32 @@
 import os
 import pygame
+import random
 from random import randint
 
 pygame.mixer.init()
 pygame.init()
 
-pygame.mixer.set_num_channels(26)
+pygame.mixer.set_num_channels(10)
 
 first = []
 
 second = []
 
-for (dirpath, dirnames, filenames) in os.walk("tracks/firstStereo/"):
+for (dirpath, dirnames, filenames) in os.walk("tracks/first/"):
     for filename in filenames:
         filepath = dirpath + '/' + filename
         first.append(pygame.mixer.Sound(filepath))
 
-for (dirpath, dirnames, filenames) in os.walk("tracks/secondStereo/"):
+for (dirpath, dirnames, filenames) in os.walk("tracks/second/"):
     for filename in filenames:
         filepath = dirpath + '/' + filename
         second.append(pygame.mixer.Sound(filepath))
 
 
-def play(x, channel, track):
-    pygame.mixer.Channel(channel).play(track, loops=0, maxtime=x)
+def play(channel, track):
+    pygame.mixer.Channel(channel).play(track, loops=0)
+    r = random.uniform(0, 1)
+    pygame.mixer.Channel(channel).set_volume(r, (1 - r))
 
 
 while True:
@@ -32,19 +35,20 @@ while True:
     k = randint(0, 2)
     l = randint(0, 9)
 
-    play(10000, 0, first[i])
+
+    play(0, first[i])
     pygame.time.wait(int(round(first[i].get_length(), 3) * 1000) + 300)
-    play(10000, 0, second[j])
+    play(0, second[j])
     pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
 
     if l == 0:
         for a in range(0, 4):
-            play(10000, 0, second[j])
+            play(0, second[j])
             pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
     else:
         while k > 0:
             j = randint(0, len(second) - 1)
-            play(10000, 0, second[j])
+            play(0, second[j])
             pygame.time.wait(int(round(second[j].get_length(), 3) * 1000) + 300)
             k = randint(0, 2)
 
